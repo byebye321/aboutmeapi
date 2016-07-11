3c64ca81bd13855ce21b58deb36951025387b44f
stephanie <hendricks.step@gmail.com>
2016-07-11 20:21:45 +0800

    added profile

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.listen(port, function (){
+//this is a generic route so this will always be shown on the cosole log
+app.use('/', (req, res, next) =>{
+   console.log('hi')
+   next()
+})
+
+app.use('/profile', (req, res) =>{
+  const name = {
+    name: 'Stephanie',
+    age: 5,
+    pet: 'a dog named dog',
+    gender: 'female',
+  }
+  res.status(200).json(name)
+})
+
+app.listen(port, () => {
   console.log(`listening to port ${port}`)
 })
